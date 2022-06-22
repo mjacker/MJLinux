c53cac0328f57156b501a63c4cebd865fd5814ca
MJKali <mjackerjaime@gmail.com>
2022-06-22 05:46:59 -0400

    Added run colors.

---
+++ 01 Bash/03 Colors/runColors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in {1..107}
+do
+	echo -e "\e[$i""m Color number #$i. \e[0m"
+done
+
+#echo -e "\e[32m Color number #32. \e[0m"
+#echo -e "\e[33m Color number #33. \e[0m"
+#echo -e "\e[34m Color number #34. \e[0m"
+#echo -e "\e[35m Color number #35. \e[0m"
+#echo -e "\e[36m Color number #36. \e[0m"
+#
+# combination color
+#echo -e "\e[1;33;4;44mYellow Underlined Text on Blue Background\e[0m"
